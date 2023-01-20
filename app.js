3c98daac5d5e32897c6f79685aa93e5a0e996a30
a107studio <109347193+a107studio@users.noreply.github.com>
2023-01-20 20:24:55 +0800

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var htmlWidth =document.documentElement.clientWidth || document.body.clientWidth;
-var htmlDom =document.getElementsByTagName('html')[0];
-htmlDom.style.fontSize=htmlWidth / 10+'px';
-window.addEventListener('resize',function(){
-	var htmlWidth =document.documentElement.clientWidth || document.body.clientWidth;
-	htmlDom.style.fontSize=htmlWidth / 10+'px';
-})
